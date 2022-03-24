e46e4743eda1f7588da5b095ebdfe9c9030234a6
Arita Osmani <aritaaosmani@gmail.com>
2022-03-24 22:46:46 +0100

    Changed some styling at header and signup page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../styles/SignUp.css'
-import { SignUpFrom, User } from '../types'
+import { SignUpFrom, User } from '../Types'
 
 type Props = {
     user: User | null,
